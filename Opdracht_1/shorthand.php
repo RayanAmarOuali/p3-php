8a43ac1c665470d9ab0d60ec9800e491044c3817
RayanAmarOuali <9021944@student.zadkine.nl>
2026-02-24 14:11:25 +0100

    Initial commit

---
+++ Opdracht_1/shorthand.php
@@ -0,0 +1,30 @@
+<?php 
+   $Naam = "Henk";
+   $leeftijd = 18;
+   $programmeertaal = "PHP";
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+  <p>Mijn naam is <?= $Naam; ?></p>
+  <p>Mijn leeftijd is <?= $leeftijd; ?></p>
+  <p>Mijn favoriete programmeertaal is <?= $programmeertaal; ?></p>
+
+  
+  
+  
+
+
+    
+</body>
+</html>
